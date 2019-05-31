8ab75dffca5b5ca4f381b3d890836bacae0daf21
gregga <gregg.acheson@oslosolutions.com>
2019-05-31 01:32:48 -0700

    ps1 pyver fns

--- scripts/setup/setup-python36.ps1
+++ scripts/setup/setup-python36.ps1
@@ -4,42 +4,71 @@
 # full license information.
 #
 
-Write-Host "POWERSHELL SCRIPT in Setup-Python36" -ForegroundColor Red -BackgroundColor Yellow
+function CheckVerString([string] $pyVerStr) {
+
+    $PyVer = $pyVerStr.Split('.')
+    $PvMaj = [int]$PyVer[0]
+    $PvMin = [int]$PyVer[1]
+
+    if($PvMaj -gt $PythonMinVersionMajor)
+    {
+        Write-Host "Found Python Version: " $pyVerStr -ForegroundColor Green
+        exit 0
+    }
+    if($PvMaj -eq $PythonMinVersionMajor)
+    {
+        if($PvMin -ge $PythonMinVersionMinor)
+        {
+            Write-Host "Found Python Version: " $pyVerStr -ForegroundColor Green
+            exit 0
+        }
+    }
+}
+
+
+function SearchForPythonVersion()
+{
+    Write-Host "Checking for python version (minimum): $PythonMinVersionMajor.$PythonMinVersionMinor" -ForegroundColor Yellow
+
+    try {
+        $PyVerCmd = & python -V 2>&1
+        $PyParts = $PyVerCmd.split(' ')
+        CheckVerString($PyParts[1])
+    }
+    catch {
+        Write-Host "Looking for python in path..." -ForegroundColor Yellow
+    }
+    
+    $PyPath = Which("python*")
+    
+    if ($PyPath) {
+        foreach($PyFile in $PyPath)
+        {
+            try {
+                $PyVer = (Get-Item $PyFile).VersionInfo
+                CheckVerString($PyVer, $vMajor, $vMinor)
+            }
+            catch {
+                Write-Host "Looking for python in path..." -ForegroundColor Yellow
+            }
+        }
+    }
+    else {
+        Write-Host "python not found" -ForegroundColor Red
+        exit 1
+    }    
+}
 
 function Which([string] $cmd) {
     $path = (($Env:Path).Split(";") | Select -uniq | Where { $_.Length } | Where { Test-Path $_ } | Get-ChildItem -filter $cmd).FullName
     return $path
-  }
-
-Write-Host "checking for python 3.5+" -ForegroundColor Yellow
-
-$PyPath = Which("python*")
-
-if ($PyPath)
-{
-    $PyVerCmd = & python -V 2>&1
-    $PyParts = $PyVerCmd.split(' ')
-    $PyVer = $PyParts[1].split('.')
-    $PvMaj = [int]$PyVer[0]
-    $PvMin = [int]$PyVer[1]
-    $PvBld = [int]$PyVer[2]
-    Write-Host $PyVer -ForegroundColor Blue
-    foreach($PyFile in $PyPath)
-    {
-        $PyVer = (Get-Item $PyFile).VersionInfo
-        Write-Host $PyFile "::VER::" $PyVer.FileVersion -ForegroundColor Green
-    }
-    exit 0
-}
-else {
-    Write-Host "python not found" -ForegroundColor Yellow
-    exit 1
 }
 
 
+$PythonMinVersionMajor = 3
+$PythonMinVersionMinor = 5
 
-
-
-
+Write-Host "POWERSHELL SCRIPT in Setup-Python36" -ForegroundColor Red -BackgroundColor Yellow
+SearchForPythonVersion($PythonMinVersionMajor, $PythonMinVersionMinor)
 
 
