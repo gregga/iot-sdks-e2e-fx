f1b4c9dbc64c8406ac3d0b15c8453cf87508ed49
gregga <gregg.acheson@oslosolutions.com>
2019-05-30 22:09:27 -0700

    powershell ver check

--- scripts/setup/setup-python36.ps1
+++ scripts/setup/setup-python36.ps1
@@ -6,7 +6,37 @@
 
 Write-Host "POWERSHELL SCRIPT in Setup-Python36" -ForegroundColor Red -BackgroundColor Yellow
 
+function Which([string] $cmd) {
+    $path = (($Env:Path).Split(";") | Select -uniq | Where { $_.Length } | Where { Test-Path $_ } | Get-ChildItem -filter $cmd).FullName
+    return $path
+  }
 
+  
+
+
+
+Write-Host "checking for python 3.5+" -ForegroundColor Yellow
+
+$PyPath = Which("python*")
+
+if ($PyPath)
+{
+    $PyVerCmd = & python -V 2>&1
+    $PyParts = $PyVerCmd.split(' ')
+    $PyVer = $PyParts[1].split('.')
+    $PvMaj = [int]$PyVer[0]
+    $PvMin = [int]$PyVer[1]
+    $PvBld = [int]$PyVer[2]
+    Write-Host $PyVer -ForegroundColor Blue
+    foreach($PyFile in $PyPath)
+    {
+        $PyVer = (Get-Item $PyFile).VersionInfo
+        Write-Host $PyFile "::VER::" $PyVer.FileVersion -ForegroundColor Green
+    }
+}
+else {
+    Write-Host "python not found" -ForegroundColor Yellow
+}
 
 
 
