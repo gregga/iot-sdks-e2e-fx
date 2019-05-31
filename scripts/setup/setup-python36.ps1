2e371ec3ba4bc07279c0fd315da50ed075703fef
gregga <gregg.acheson@oslosolutions.com>
2019-05-30 20:18:41 -0700

    launch PS1 in pre-test

---
+++ scripts/setup/setup-python36.ps1
@@ -0,0 +1,17 @@
+#!/usr/bin/env powershell
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for
+# full license information.
+#
+
+Write-Host "POWERSHELL SCRIPT in Setup-Python36" -ForegroundColor Red -BackgroundColor Yellow
+
+
+
+
+
+
+
+
+
+
