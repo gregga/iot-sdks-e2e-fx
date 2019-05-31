d3aa90c1a1fd6a3e6d396a12437695c2fe973c6e
gregga <gregg.acheson@oslosolutions.com>
2019-05-31 11:01:58 -0700

    find and ver check python

--- scripts/setup/setup-python36.ps1
+++ scripts/setup/setup-python36.ps1
@@ -13,49 +13,62 @@ function CheckVerString([string] $pyVerStr) {
     if($PvMaj -gt $PythonMinVersionMajor)
     {
         Write-Host "Found Python Version: " $pyVerStr -ForegroundColor Green
-        exit 0
+        return $true
     }
     if($PvMaj -eq $PythonMinVersionMajor)
     {
         if($PvMin -ge $PythonMinVersionMinor)
         {
             Write-Host "Found Python Version: " $pyVerStr -ForegroundColor Green
-            exit 0
+            return $true
         }
     }
+    return $false
 }
-
 
 function SearchForPythonVersion()
 {
     Write-Host "Checking for python version (minimum): $PythonMinVersionMajor.$PythonMinVersionMinor" -ForegroundColor Yellow
 
     try {
-        $PyVerCmd = & python -V 2>&1
+        $PyVerCmd = & python3x -V 2>&1
         $PyParts = $PyVerCmd.split(' ')
-        CheckVerString($PyParts[1])
+        return CheckVerString($PyParts[1])
     }
     catch {
-        Write-Host "Looking for python in path..." -ForegroundColor Yellow
+        try {
+            $PyVerCmd = & pythonx -V 2>&1
+            $PyParts = $PyVerCmd.split(' ')
+            return CheckVerString($PyParts[1])
+        }
+        catch {
+            Write-Host "Looking for python in path..." -ForegroundColor Yellow
+        }
     }
     
     $PyPath = Which("python*")
-    
+    $prog = '.'
     if ($PyPath) {
         foreach($PyFile in $PyPath)
         {
             try {
                 $PyVer = (Get-Item $PyFile).VersionInfo
-                CheckVerString($PyVer, $vMajor, $vMinor)
+                $pyFound =  CheckVerString($PyVer.FileVersion)
+                if($pyFound)
+                {
+                    Write-Host $PyFile -ForegroundColor Green
+                    return $true
+                }
             }
             catch {
-                Write-Host "Looking for python in path..." -ForegroundColor Yellow
+                Write-Host $prog -ForegroundColor Yellow
             }
+            $prog += '.'
         }
     }
     else {
         Write-Host "python not found" -ForegroundColor Red
-        exit 1
+        return $false
     }    
 }
 
@@ -69,6 +82,17 @@ $PythonMinVersionMajor = 3
 $PythonMinVersionMinor = 5
 
 Write-Host "POWERSHELL SCRIPT in Setup-Python36" -ForegroundColor Red -BackgroundColor Yellow
-SearchForPythonVersion($PythonMinVersionMajor, $PythonMinVersionMinor)
+$foundPy = SearchForPythonVersion($PythonMinVersionMajor, $PythonMinVersionMinor)
 
+if(!$foundPy)
+{
+    if ([System.Boolean](Get-CimInstance -ClassName Win32_OperatingSystem -ErrorAction SilentlyContinue)) {
+        Write-Host "NEED to install python 3.6 on Windows." -ForegroundColor Red
+        exit 1
+    }
+    else {
+        Write-Host "Installing python 3.6..." -ForegroundColor Green
+    }
+}
 
+Write-Host "setup-python 3.6 SUCCESS" -ForegroundColor Green
