683f1e018a0994826577ab21d4a6808028723ebe
gregga <gregg.acheson@oslosolutions.com>
2019-06-01 13:35:16 -0700

    use sudo

--- scripts/setup/setup-python36.ps1
+++ scripts/setup/setup-python36.ps1
@@ -321,7 +321,7 @@ if($IsWin32) {
     $out = python -m pip install python_glue
 }
 else{
-    $out = python3 -m $pipcmd install python_glue
+    $out = sudo python3 -m pip install python_glue
 }
 if($out.Length -gt 0){
     foreach($o in $out){
@@ -360,7 +360,7 @@ if($IsWin32) {
     $out = python -m $pipcmd install horton_helpers
 }
 else{
-    $out = python3 -m $pipcmd install horton_helpers
+    $out = python3 -m pip install horton_helpers
 }
 if($out.Length -gt 0){
     foreach($o in $out){
