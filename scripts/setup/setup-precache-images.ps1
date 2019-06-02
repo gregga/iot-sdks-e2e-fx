cc16cb794160a57f61c307e583ddc845ae7bf2c2
gregga <gregg.acheson@oslosolutions.com>
2019-06-01 21:33:18 -0700

    pass params

--- scripts/setup/setup-precache-images.ps1
+++ scripts/setup/setup-precache-images.ps1
@@ -4,6 +4,12 @@
 Param
 (
     [Parameter(Position=0)]
+    [string]$Repo_User,
+    [Parameter(Position=0)]
+    [string]$Repo_Pw,
+    [Parameter(Position=0)]
+    [string]$Repo_Name,
+    [Parameter(Position=0)]
     [string]$Language,
     [Parameter(Position=1)]
     [string]$Test_Image,
@@ -25,7 +31,7 @@ Write-Host "RealPath $path" -ForegroundColor Yellow
 
 
 
-docker login -u $env:IOTHUB_E2E_REPO_USER -p $env:IOTHUB_E2E_REPO_PASSWORD $env:IOTHUB_E2E_REPO_ADDRESS
+docker login -u $Repo_User -p $Repo_Pw $Repo_Name
 docker pull $env:IOTHUB_E2E_REPO_ADDRESS/$Language-e2e-v3:$Test_Image
 docker pull $Image_EdgeAgent
 docker pull $Image_EdgeHub
