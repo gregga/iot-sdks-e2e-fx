9aaefb1a9d11ec841b6ca9bc46f5905ecb6d61b5
gregga <gregg.acheson@oslosolutions.com>
2019-06-02 21:14:04 -0700

    powershell post steps

---
+++ scripts/remove-edgehub-device.ps1
@@ -0,0 +1,15 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+$script_dir = $pwd.Path
+$path = $MyInvocation.MyCommand.Path
+if (!$path) {$path = $psISE.CurrentFile.Fullpath}
+if ( $path) {$path = split-path $path -Parent}
+set-location $path
+Write-Host "RealPath $path" -ForegroundColor Yellow
+$pysripts = Join-Path -Path $path -ChildPath '../pyscripts' -Resolve
+
+$out = sudo -H -E python3 $pysripts/remove_edgehub_device.py
+foreach($o in $out){
+    Write-Host $o -ForegroundColor Blue
+}
