dc9f08556c4742d606b198b638e79c065957ecfb
gregga <gregg.acheson@oslosolutions.com>
2019-06-04 13:15:39 -0700

    fix dirpath

--- scripts/setup/setup-iotedge.ps1
+++ scripts/setup/setup-iotedge.ps1
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-$script_dir = $pwd.Path
+#$script_dir = $pwd.Path
 $path = $MyInvocation.MyCommand.Path
 if (!$path) {$path = $psISE.CurrentFile.Fullpath}
 if ( $path) {$path = split-path $path -Parent}
