b721835cb0138d7b12913fbf677ef1205be96d5b
gregga <gregg.acheson@oslosolutions.com>
2019-06-05 04:50:46 -0700

    arg hack

--- scripts/setup/setup-precache-images.ps1
+++ scripts/setup/setup-precache-images.ps1
@@ -3,26 +3,49 @@
 
 Param
 (
-    [Parameter(Mandatory=$false)]
-    [Alias("lang")] 
-    [string]$language="",
+    [Parameter(Position=0)]
+    [AllowEmptyString()]
+    [string]$language="0",
 
-    [Parameter(Mandatory=$false)]
-    [Alias("timg")] 
-    [string]$test_image="",
-
-    [Parameter(Mandatory=$false)]
-    [Alias("eaimg")] 
-    [string]$image_edgeagent="",
-
-    [Parameter(Mandatory=$false)]
-    [Alias("ehimg")] 
-    [string]$image_edgehub="",
-
-    [Parameter(Mandatory=$false)]
-    [Alias("frImg")] 
-    [string]$image_friendmod=""
+    [Parameter(Position=1)]
+    [AllowEmptyString()]
+    [string]$test_image="1",
+    
+    [Parameter(Position=2)]
+    [AllowEmptyString()]
+    [string]$image_edgeagent="2",
+    
+    [Parameter(Position=3)]
+    [AllowEmptyString()]
+    [string]$image_edgehub="3",
+    
+    [Parameter(Position=4)]
+    [AllowEmptyString()]
+    [string]$image_friendmod="4"
 )
+
+#Param
+#(
+#    [Parameter(Mandatory=$false)]
+#    [Alias("lang")] 
+#    [string]$language="",#
+
+#   [Parameter(Mandatory=$false)]
+#    [Alias("timg")] 
+#    [string]$test_image="",
+
+#    [Parameter(Mandatory=$false)]
+#    [Alias("eaimg")] 
+#    [string]$image_edgeagent="",
+
+#    [Parameter(Mandatory=$false)]
+#    [Alias("ehimg")] 
+#    [string]$image_edgehub="",
+
+#    [Parameter(Mandatory=$false)]
+#    [Alias("frImg")] 
+#    [string]$image_friendmod=""
+#)
 
 $horton_user = $env:IOTHUB_E2E_REPO_USER
 $horton_pw = $env:IOTHUB_E2E_REPO_PASSWORD
