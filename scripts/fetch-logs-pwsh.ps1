e281b94e1c60b737daac0a43e50e96e6de4b8ba2
gregga <gregg.acheson@oslosolutions.com>
2019-06-06 21:21:27 -0700

    suppoer inject-junit on Win32

--- scripts/fetch-logs-pwsh.ps1
+++ scripts/fetch-logs-pwsh.ps1
@@ -125,7 +125,7 @@ if($isWin32) {
     $out = python ${root_dir}/pyscripts/inject_into_junit.py -junit_file $junit_file -log_file merged.log; if ($LASTEXITCODE -ne 0) { Write-Host "error injecting into junit" -ForegroundColor Red; $out }
 }
 else {
-    $out = python3 python ${root_dir}/pyscripts/inject_into_junit.py -junit_file $junit_file -log_file merged.log; if ($LASTEXITCODE -ne 0) { Write-Host "error injecting into junit" -ForegroundColor Red; $out }
+    $out = python3 ${root_dir}/pyscripts/inject_into_junit.py -junit_file $junit_file -log_file merged.log; if ($LASTEXITCODE -ne 0) { Write-Host "error injecting into junit" -ForegroundColor Red; $out }
 }
 $out | Out-File -Append $resultsdir/${mod}.log
 
