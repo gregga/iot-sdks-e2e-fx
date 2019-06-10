1f7d494b77fb388904afaeae9a84dd17c308e787
gregga <gregg.acheson@oslosolutions.com>
2019-06-09 22:01:30 -0700

    spaced out args

--- scripts/build-docker-image.ps1
+++ scripts/build-docker-image.ps1
@@ -43,10 +43,10 @@ else {
 }
 
 $args = ""
-if("$language" -ne "") { $args += "--language $language"}
-if("$repo" -ne "") { $args += "--repo $repo"}
-if("$commit" -ne "") { $args += "--commit $commit"}
-if("$variant" -ne "") { $args += "--variant $variant"}
+if("$language" -ne "") { $args += "--language $language "}
+if("$repo" -ne "") { $args += "--repo $repo "}
+if("$commit" -ne "") { $args += "--commit $commit "}
+if("$variant" -ne "") { $args += "--variant $variant "}
 
 Write-Host "build-docker-image $args"
 
