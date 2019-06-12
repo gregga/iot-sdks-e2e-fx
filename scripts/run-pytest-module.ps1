99f6f326b733f1ca9d20e4480580cd099233d9d1
gregga <gregg.acheson@oslosolutions.com>
2019-06-11 19:00:14 -0700

    rm extraargs

--- scripts/run-pytest-module.ps1
+++ scripts/run-pytest-module.ps1
@@ -68,4 +68,4 @@ if( "$EncodedText" -ne "") {
 
 set-location $testpath
 write-host "pytest -v --scenario $test_scenario --transport=$test_transport --$test_lang-wrapper --junitxml=$test_junitxml -o $test_o $test_extra_args"
-$py = PyCmd " -u -m pytest -v --scenario $test_scenario --transport=$test_transport --$test_lang-wrapper --junitxml=$test_junitxml -o $test_o $test_extra_args"; Invoke-Expression  $py
+$py = PyCmd " -u -m pytest -v --scenario $test_scenario --transport=$test_transport --$test_lang-wrapper --junitxml=$test_junitxml -o $test_o"; Invoke-Expression  $py
